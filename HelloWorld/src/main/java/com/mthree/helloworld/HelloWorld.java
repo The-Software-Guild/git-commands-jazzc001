8171fcb95a4d077772de06a7955889a6845b8e25
jazzc001 <jazzcheung212@gmail.com>
2022-10-11 15:08:00 +0100

    Hello world project in NetBeans

---
+++ HelloWorld/src/main/java/com/mthree/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mthree.helloworld;
+
+/**
+ *
+ * @author jasminecheung
+ * @email: jazzcheung212@gmail.com
+ * date: 11/10/2022
+ * purpose: My first Hello World program in an IDE
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
