4775260fa24e81505b6d6b8309c1b9605b4c030f
jazzc001 <jazzcheung212@gmail.com>
2022-10-13 10:35:51 +0100

    finished ARainbow exercise

---
+++ HelloWorld/src/main/java/com/sg/foundations/basics/arrays/ARainbow.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.foundations.basics.arrays;
+
+/**
+ *
+ * @author jasminecheung
+ */
+public class ARainbow {
+    
+    public static void main(String[] args) {
+        String[] colors = {"Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"};
+        
+        for (String color : colors) {
+            System.out.println(color);
+        }
+    }
+    
+}
