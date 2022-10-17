9dddf6a34c38d3d3940a19fdc48bf1a5854a3703
jazzc001 <jazzcheung212@gmail.com>
2022-10-17 03:48:07 +0100

    finished picky eater

--- HelloWorld/src/main/java/com/sg/foundations/flowcontrol/ifs/KnockKnock.java
+++ HelloWorld/src/main/java/com/sg/foundations/flowcontrol/ifs/KnockKnock.java
@@ -18,12 +18,16 @@ public class KnockKnock {
 
         System.out.print("Knock Knock! Guess who!! ");
         String nameGuess = inputReader.nextLine();
-
+        
+        //== checks if both objects point to the same memory location whereas . equals() evaluates to the comparison of values in the objects.
         if(nameGuess.equals("Marty McFly")){
             System.out.println("Hey! That's right! I'm back!");
             System.out.println(".... from the Future."); // Sorry, had to!
         }else{
             System.out.println("Dude, do I -look- like " + nameGuess);
         }
+        
+        //What happens if you don't type it in the right capitalization? How might you fix that?
+        //use string manupliation , use '.toLowerCase()' to compare the user input to the statement
     }
 }
